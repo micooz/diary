a79bb006d1298e8b0a45c3c0a2b41b1beb4c36c8
Micooz <micooz@hotmail.com>
2016-04-01 21:24:19 +0800

    chore(compiler): update config.js

--- config.js
+++ config.js
@@ -1,6 +1,22 @@
+import path from 'path';
+
+const root = function root() {
+  return path.resolve.apply(path, arguments);
+};
+
 /**
  * configurations
  */
 export default {
+
+  /**
+   * where to store *.md
+   */
+  archives: root('archives'),
+
+  /**
+   * where to put compiled static pages (*.html)
+   */
+  dist: root('-')
 
 };
