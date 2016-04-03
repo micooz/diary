c07c7753227b1e34e1c1c5b3d98c9fb283e97ba4
Micooz <micooz@hotmail.com>
2016-04-03 23:01:23 +0800

    chore(html): add Google Analytics in production

--- src/compiler/components/html.component.js
+++ src/compiler/components/html.component.js
@@ -29,6 +29,17 @@ export class Html extends Component {
         <script src="/app.js"></script> :
         <script src="/diary/dist/app.min.js"></script>
       }
+      {__PRODUCTION__ ?
+        <script dangerouslySetInnerHTML={{__html: `
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-72182315-2', 'auto');
+          ga('send', 'pageview');
+      `}}>
+        </script> : null}
       </body>
       </html>
     );
