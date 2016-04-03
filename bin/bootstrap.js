d558ce3c45bddada8d33db223db85f977cd90c0e
Micooz <micooz@hotmail.com>
2016-04-03 21:15:27 +0800

    feat(bootstrap): use piping

--- bin/bootstrap.js
+++ bin/bootstrap.js
@@ -1,6 +1,20 @@
 #!/usr/bin/env node
 require('babel-core/register');
-require('../src/compiler');
+
+const __DEVELOPMENT__ = process.env.NODE_ENV !== 'production';
+
+global.__DEVELOPMENT__ = __DEVELOPMENT__;
+global.__PRODUCTION__ = !__DEVELOPMENT__;
+
+if (__DEVELOPMENT__) {
+  if (require('piping')({
+      hook: true
+    })) {
+    require('../src/compiler');
+  }
+} else {
+  require('../src/compiler');
+}
 
 // necessary for concurrently
 return 0;
